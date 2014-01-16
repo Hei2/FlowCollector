b0a62e169456a596155bfa20406c2dbe2c8d0525
Hei2 <kjsalveson@hotmail.com>
2014-01-15 23:01:31 -0600

    Packet Capturing
    Packets for version 1, 5, 6, and 7 should be read (assuming no padding
    on the front of the packet). Need to determine what fields should be
    collected. Once fields are agreed upon, need to actually save the fields
    to a database.

--- NetFlow/src/netflow/PacketCapture.java
+++ NetFlow/src/netflow/PacketCapture.java
@@ -41,13 +41,82 @@ public class PacketCapture extends Thread
             DataInputStream in = new DataInputStream(byteIn);
 
             short version = in.readShort();
+            short count = in.readShort();
+            in.skipBytes(4);
+            int unix_secs = in.readInt();
             
+            /*
+             * Depending on the version of the NetFlow packet, grab the 
+             * relevant information.
+             * !!DETERMINE WHAT NEEDS TO BE COLLECTED!!
+             */
             if (version == 9)
             {
-                short count = in.readShort();
-                in.skipBytes(4);
-                int unix_secs = in.readInt();
                 //in.skipBytes();
+            }
+            else if (version == 8)
+            {
+                
+            }
+            else if (version == 1)
+            {
+                in.skipBytes(5); //Skip unix_nsecs
+                for (int i = 0; i < count; i++)
+                {
+                    int srcaddr = in.readInt();
+                    int dstaddr = in.readInt();
+                    in.skipBytes(24); //Skip nexthop through last
+                    short srcport = in.readShort();
+                    short dstport = in.readShort();
+                    in.skipBytes(2); //Skip pad1
+                    byte prot = in.readByte();
+                    in.skipBytes(10); //Ignoring tos through pad2
+                }
+            }
+            else if (version == 5)
+            {
+                in.skipBytes(12); //Skip unix_nsecs through sampling_interval
+                for (int i = 0; i < count; i++)
+                {
+                    int srcaddr = in.readInt();
+                    int dstaddr = in.readInt();
+                    in.skipBytes(24); //Skip nexthop through last
+                    short srcport = in.readShort();
+                    short dstport = in.readShort();
+                    in.skipBytes(2); //Skip pad1 through tcp_flags
+                    byte prot = in.readByte();
+                    in.skipBytes(9); //Ignoring tos through pad2
+                }
+            }
+            else if (version == 6) //!!CONSIDER CONSOLIDATING VERSIONS 6 & 7!!
+            {
+                in.skipBytes(12); //Skip unix_nsecs through sampling_interval
+                for (int i = 0; i < count; i++)
+                {
+                    int srcaddr = in.readInt();
+                    int dstaddr = in.readInt();
+                    in.skipBytes(24); //Skip nexthop through last
+                    short srcport = in.readShort();
+                    short dstport = in.readShort();
+                    in.skipBytes(2); //Skip pad1 through tcp_flags
+                    byte prot = in.readByte();
+                    in.skipBytes(13); //Ignoring tos through pad2
+                }
+            }
+            else if (version == 7)
+            {
+                in.skipBytes(12); //Skip unix_nsecs through reserved
+                for (int i = 0; i < count; i++)
+                {
+                    int srcaddr = in.readInt();
+                    int dstaddr = in.readInt();
+                    in.skipBytes(24); //Skip nexthop through last
+                    short srcport = in.readShort();
+                    short dstport = in.readShort();
+                    in.skipBytes(2); //Skip pad1 through tcp_flags
+                    byte prot = in.readByte();
+                    in.skipBytes(13); //Ignoring tos through router_sc
+                }
             }
         }
         catch (IOException ex)
