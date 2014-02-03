328bbed79452883ba61da0ea99009d817dc52841
Hei2 <kjsalveson@hotmail.com>
2014-02-02 18:49:27 -0600

    PacketV1 Database Connection Integrated
    Modified CreateDatabase() in Main class to handle its own exceptions.
    url, user, and pass are now read from config.txt in the NetFlow
    directory.
    PacketV1 now has INSERT statements.

--- NetFlow/src/netflow/Main.java
+++ NetFlow/src/netflow/Main.java
@@ -1,14 +1,16 @@
 package netflow;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-//import java.util.Scanner;
+import java.util.Scanner;
 
 //http://stackoverflow.com/questions/5160414/read-netflow-rflow-dd-wrt-packet-content
 //http://stackoverflow.com/questions/10556829/sending-and-receiving-udp-packets-using-java
-//http://www.vogella.com/tutorials/MySQLJava/article.html
+//http://dev.mysql.com/doc/refman/5.0/es/connector-j-reference-configuration-properties.html
 
 /**
  *
@@ -17,6 +19,7 @@ import java.sql.Statement;
 public class Main
 {
     private static String user, pass, url;
+    
     
     public static String GetUser() { return user; }
     
@@ -35,183 +38,223 @@ public class Main
         scanner.close();
         System.out.println("\nThe name associated with that IP is:\n" + Functions.getHostName(ip) + "\n");*/
         
-        //Do something to get the url, user, and password from the user.
+        //Get the url, user, and password from the user.
+        //Consider reading a configuration file.
+        File config = new File("config.txt");
         
-        try
+        try (Scanner scanner = new Scanner(config);)
         {
-            CreateDatabase();
+            while (scanner.hasNextLine())
+            {
+                String line = scanner.nextLine();
+                if (line.startsWith("URL="))
+                {
+                    url = line.substring(4);
+                }
+                else if (line.startsWith("USER="))
+                {
+                    user = line.substring(5);
+                }
+                else if (line.startsWith("PASS="))
+                {
+                    pass = line.substring(5);
+                }
+            }
         }
-        catch (SQLException | ClassNotFoundException ex)
+        catch (FileNotFoundException ex)
         {
-            System.out.println("Failed to create the database tables.\nRestart the program.");
             System.out.println(ex.getMessage());
+            System.out.println("Error: FileNotFoundException occurred in Main.");
         }
         
-        Capture capture = new Capture();
-        capture.start();
+        if (CreateDatabase())
+        {
+            Capture capture = new Capture();
+            capture.start();
+        }
     }
     
-    private static void CreateDatabase()
-    throws SQLException, ClassNotFoundException
+    private static boolean CreateDatabase()
     {
-        //Load the driver.
-        Class.forName("com.mysql.jdbc.Driver");
-        
-        //Create a connection.
-        Connection conn = DriverManager.getConnection(url, user, pass);
-        
-        //Create a statement object to use.
-        Statement stmt = conn.createStatement();
-        
-        //Begin creating tables.
-        String create = "CREATE TABLE IF NOT EXISTS PACKET_V1_HEADER (id INT NOT NULL AUTO_INCREMENT, " +
-                        "count SMALLINT NOT NULL, " +
-                        "sys_uptime INT NOT NULL, " +
-                        "unix_secs INT NOT NULL, " +
-                        "unix_nsecs INT NOT NULL, " +
-                        "PRIMARY KEY (id));";
-        stmt.executeUpdate(create);
-        
-        create = "CREATE TABLE IF NOT EXISTS PACKET_V1 (id INT NOT NULL AUTO_INCREMENT, " +
-                        "header_id INT NOT NULL, " +
-                        "srcaddr INT NOT NULL, " +
-                        "dstaddr INT NOT NULL, " +
-                        "nexthop INT NOT NULL, " +
-                        "input SMALLINT NOT NULL, " +
-                        "output SMALLINT NOT NULL, " +
-                        "dPkts INT NOT NULL, " +
-                        "dOctets INT NOT NULL, " +
-                        "first INT NOT NULL, " +
-                        "last INT NOT NULL, " +
-                        "srcport SMALLINT NOT NULL, " +
-                        "dstport SMALLINT NOT NULL, " +
-                        "prot TINYINT NOT NULL, " +
-                        "tos TINYINT NOT NULL, " +
-                        "flags TINYINT NOT NULL, " +
-                        "PRIMARY KEY (id), " +
-                        "FOREIGN KEY(header_id) REFERENCES PACKET_V1_HEADER(id) ON DELETE CASCADE);";
-        stmt.executeUpdate(create);
-        
-        create = "CREATE TABLE IF NOT EXISTS PACKET_V5_HEADER (id INT NOT NULL AUTO_INCREMENT, " +
-                        "count SMALLINT NOT NULL, " +
-                        "sys_uptime INT NOT NULL, " +
-                        "unix_secs INT NOT NULL, " +
-                        "unix_nsecs INT NOT NULL, " +
-                        "flow_sequence INT NOT NULL, " +
-                        "engine_type TINYINT NOT NULL, " +
-                        "engine_id TINYINT NOT NULL, " +
-                        "sampling_interval SMALLINT NOT NULL, " +
-                        "PRIMARY KEY (id));";
-        stmt.executeUpdate(create);
-        
-        create = "CREATE TABLE IF NOT EXISTS PACKET_V5 (id INT NOT NULL AUTO_INCREMENT, " +
-                        "header_id INT NOT NULL, " +
-                        "srcaddr INT NOT NULL, " +
-                        "dstaddr INT NOT NULL, " +
-                        "nexthop INT NOT NULL, " +
-                        "input SMALLINT NOT NULL, " +
-                        "output SMALLINT NOT NULL, " +
-                        "dPkts INT NOT NULL, " +
-                        "dOctets INT NOT NULL, " +
-                        "first INT NOT NULL, " +
-                        "last INT NOT NULL, " +
-                        "srcport SMALLINT NOT NULL, " +
-                        "dstport SMALLINT NOT NULL, " +
-                        "tcp_flags TINYINT NOT NULL, " +
-                        "prot TINYINT NOT NULL, " +
-                        "tos TINYINT NOT NULL, " +
-                        "src_as SMALLINT NOT NULL, " +
-                        "dst_as SMALLINT NOT NULL, " +
-                        "src_mask TINYINT NOT NULL, " +
-                        "dst_mask TINYINT NOT NULL, " +
-                        "PRIMARY KEY (id), " +
-                        "FOREIGN KEY (header_id) REFERENCES PACKET_V5_HEADER(id) ON DELETE CASCADE);";
-        stmt.executeUpdate(create);
-        
-        create = "CREATE TABLE IF NOT EXISTS PACKET_V6_HEADER (id INT NOT NULL AUTO_INCREMENT, " +
-                        "count SMALLINT NOT NULL, " +
-                        "sys_uptime INT NOT NULL, " +
-                        "unix_secs INT NOT NULL, " +
-                        "unix_nsecs INT NOT NULL, " +
-                        "flow_sequence INT NOT NULL, " +
-                        "engine_type TINYINT NOT NULL, " +
-                        "engine_id TINYINT NOT NULL, " +
-                        "sampling_interval SMALLINT NOT NULL, " +
-                        "PRIMARY KEY (id));";
-        stmt.executeUpdate(create);
-        
-        create = "CREATE TABLE IF NOT EXISTS PACKET_V6 (id INT NOT NULL AUTO_INCREMENT, " +
-                        "header_id INT NOT NULL, " +
-                        "srcaddr INT NOT NULL, " +
-                        "dstaddr INT NOT NULL," +
-                        "nexthop INT NOT NULL, " +
-                        "input SMALLINT NOT NULL, " +
-                        "output SMALLINT NOT NULL, " +
-                        "dPkts INT NOT NULL, " +
-                        "dOctets INT NOT NULL, " +
-                        "first INT NOT NULL, " +
-                        "last INT NOT NULL, " +
-                        "srcport SMALLINT NOT NULL," +
-                        "dstport SMALLINT NOT NULL, " +
-                        "tcp_flags TINYINT NOT NULL, " +
-                        "prot TINYINT NOT NULL, " +
-                        "tos TINYINT NOT NULL, " +
-                        "src_as SMALLINT NOT NULL, " +
-                        "dst_as SMALLINT NOT NULL, " +
-                        "src_mask TINYINT NOT NULL, " +
-                        "dst_mask TINYINT NOT NULL, " +
-                        "PRIMARY KEY (id), " +
-                        "FOREIGN KEY (header_id) REFERENCES PACKET_V6_HEADER(id) ON DELETE CASCADE);";
-        stmt.executeUpdate(create);
-        
-        create = "CREATE TABLE IF NOT EXISTS PACKET_V7_HEADER (id INT NOT NULL AUTO_INCREMENT, " +
-                        "count SMALLINT NOT NULL, " +
-                        "sys_uptime INT NOT NULL, " +
-                        "unix_secs INT NOT NULL, " +
-                        "unix_nsecs INT NOT NULL, " +
-                        "flow_sequence INT NOT NULL, " +
-                        "PRIMARY KEY (id));";
-        stmt.executeUpdate(create);
-        
-        create = "CREATE TABLE IF NOT EXISTS PACKET_V7 (id INT NOT NULL AUTO_INCREMENT, " +
-                        "header_id INT NOT NULL, " +
-                        "srcaddr INT NOT NULL, " +
-                        "dstaddr INT NOT NULL, " +
-                        "nexthop INT NOT NULL, " +
-                        "input SMALLINT NOT NULL, " +
-                        "output SMALLINT NOT NULL, " +
-                        "dPkts INT NOT NULL, " +
-                        "dOctets INT NOT NULL, " +
-                        "first INT NOT NULL, " +
-                        "last INT NOT NULL, " +
-                        "srcport SMALLINT NOT NULL, " +
-                        "dstport SMALLINT NOT NULL, " +
-                        "tcp_flags TINYINT NOT NULL, " +
-                        "prot TINYINT NOT NULL, " +
-                        "tos TINYINT NOT NULL, " +
-                        "src_as SMALLINT NOT NULL, " +
-                        "dst_as SMALLINT NOT NULL, " +
-                        "src_mask TINYINT NOT NULL, " +
-                        "dst_mask TINYINT NOT NULL, " +
-                        "flags SMALLINT NOT NULL, " +
-                        "router_sc INT NOT NULL, " +
-                        "PRIMARY KEY (id), " +
-                        "FOREIGN KEY (header_id) REFERENCES PACKET_V7_HEADER(id) ON DELETE CASCADE);";
-        stmt.executeUpdate(create);
-        
-        /*create = "";
-        stmt.executeUpdate(create);
-        
-        create = "";
-        stmt.executeUpdate(create);
-        
-        create = "";
-        stmt.executeUpdate(create);
-        
-        create = "";
-        stmt.executeUpdate(create);*/
-        
-        stmt.close();
-        conn.close();
+        try
+        {
+            //Load the driver.
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //Create a connection.
+            Connection conn = DriverManager.getConnection("jdbc:mysql://" + url, user, pass);
+
+            //Create a statement object to use.
+            Statement stmt = conn.createStatement();
+
+            //Begin creating tables.
+            String create = "CREATE TABLE IF NOT EXISTS PACKET_V1_HEADER (id INT NOT NULL AUTO_INCREMENT, " +
+                            "count SMALLINT NOT NULL, " +
+                            "sys_uptime INT NOT NULL, " +
+                            "unix_secs INT NOT NULL, " +
+                            "unix_nsecs INT NOT NULL, " +
+                            "PRIMARY KEY (id))";
+            stmt.executeUpdate(create);
+
+            create = "CREATE TABLE IF NOT EXISTS PACKET_V1 (id INT NOT NULL AUTO_INCREMENT, " +
+                            "header_id INT NOT NULL, " +
+                            "srcaddr INT NOT NULL, " +
+                            "dstaddr INT NOT NULL, " +
+                            "nexthop INT NOT NULL, " +
+                            "input SMALLINT NOT NULL, " +
+                            "output SMALLINT NOT NULL, " +
+                            "dPkts INT NOT NULL, " +
+                            "dOctets INT NOT NULL, " +
+                            "first INT NOT NULL, " +
+                            "last INT NOT NULL, " +
+                            "srcport SMALLINT NOT NULL, " +
+                            "dstport SMALLINT NOT NULL, " +
+                            "prot TINYINT NOT NULL, " +
+                            "tos TINYINT NOT NULL, " +
+                            "flags TINYINT NOT NULL, " +
+                            "PRIMARY KEY (id), " +
+                            "FOREIGN KEY(header_id) REFERENCES PACKET_V1_HEADER(id) ON DELETE CASCADE)";
+            stmt.executeUpdate(create);
+
+            create = "CREATE TABLE IF NOT EXISTS PACKET_V5_HEADER (id INT NOT NULL AUTO_INCREMENT, " +
+                            "count SMALLINT NOT NULL, " +
+                            "sys_uptime INT NOT NULL, " +
+                            "unix_secs INT NOT NULL, " +
+                            "unix_nsecs INT NOT NULL, " +
+                            "flow_sequence INT NOT NULL, " +
+                            "engine_type TINYINT NOT NULL, " +
+                            "engine_id TINYINT NOT NULL, " +
+                            "sampling_interval SMALLINT NOT NULL, " +
+                            "PRIMARY KEY (id))";
+            stmt.executeUpdate(create);
+
+            create = "CREATE TABLE IF NOT EXISTS PACKET_V5 (id INT NOT NULL AUTO_INCREMENT, " +
+                            "header_id INT NOT NULL, " +
+                            "srcaddr INT NOT NULL, " +
+                            "dstaddr INT NOT NULL, " +
+                            "nexthop INT NOT NULL, " +
+                            "input SMALLINT NOT NULL, " +
+                            "output SMALLINT NOT NULL, " +
+                            "dPkts INT NOT NULL, " +
+                            "dOctets INT NOT NULL, " +
+                            "first INT NOT NULL, " +
+                            "last INT NOT NULL, " +
+                            "srcport SMALLINT NOT NULL, " +
+                            "dstport SMALLINT NOT NULL, " +
+                            "tcp_flags TINYINT NOT NULL, " +
+                            "prot TINYINT NOT NULL, " +
+                            "tos TINYINT NOT NULL, " +
+                            "src_as SMALLINT NOT NULL, " +
+                            "dst_as SMALLINT NOT NULL, " +
+                            "src_mask TINYINT NOT NULL, " +
+                            "dst_mask TINYINT NOT NULL, " +
+                            "PRIMARY KEY (id), " +
+                            "FOREIGN KEY (header_id) REFERENCES PACKET_V5_HEADER(id) ON DELETE CASCADE)";
+            stmt.executeUpdate(create);
+
+            create = "CREATE TABLE IF NOT EXISTS PACKET_V6_HEADER (id INT NOT NULL AUTO_INCREMENT, " +
+                            "count SMALLINT NOT NULL, " +
+                            "sys_uptime INT NOT NULL, " +
+                            "unix_secs INT NOT NULL, " +
+                            "unix_nsecs INT NOT NULL, " +
+                            "flow_sequence INT NOT NULL, " +
+                            "engine_type TINYINT NOT NULL, " +
+                            "engine_id TINYINT NOT NULL, " +
+                            "sampling_interval SMALLINT NOT NULL, " +
+                            "PRIMARY KEY (id))";
+            stmt.executeUpdate(create);
+
+            create = "CREATE TABLE IF NOT EXISTS PACKET_V6 (id INT NOT NULL AUTO_INCREMENT, " +
+                            "header_id INT NOT NULL, " +
+                            "srcaddr INT NOT NULL, " +
+                            "dstaddr INT NOT NULL," +
+                            "nexthop INT NOT NULL, " +
+                            "input SMALLINT NOT NULL, " +
+                            "output SMALLINT NOT NULL, " +
+                            "dPkts INT NOT NULL, " +
+                            "dOctets INT NOT NULL, " +
+                            "first INT NOT NULL, " +
+                            "last INT NOT NULL, " +
+                            "srcport SMALLINT NOT NULL," +
+                            "dstport SMALLINT NOT NULL, " +
+                            "tcp_flags TINYINT NOT NULL, " +
+                            "prot TINYINT NOT NULL, " +
+                            "tos TINYINT NOT NULL, " +
+                            "src_as SMALLINT NOT NULL, " +
+                            "dst_as SMALLINT NOT NULL, " +
+                            "src_mask TINYINT NOT NULL, " +
+                            "dst_mask TINYINT NOT NULL, " +
+                            "PRIMARY KEY (id), " +
+                            "FOREIGN KEY (header_id) REFERENCES PACKET_V6_HEADER(id) ON DELETE CASCADE)";
+            stmt.executeUpdate(create);
+
+            create = "CREATE TABLE IF NOT EXISTS PACKET_V7_HEADER (id INT NOT NULL AUTO_INCREMENT, " +
+                            "count SMALLINT NOT NULL, " +
+                            "sys_uptime INT NOT NULL, " +
+                            "unix_secs INT NOT NULL, " +
+                            "unix_nsecs INT NOT NULL, " +
+                            "flow_sequence INT NOT NULL, " +
+                            "PRIMARY KEY (id))";
+            stmt.executeUpdate(create);
+
+            create = "CREATE TABLE IF NOT EXISTS PACKET_V7 (id INT NOT NULL AUTO_INCREMENT, " +
+                            "header_id INT NOT NULL, " +
+                            "srcaddr INT NOT NULL, " +
+                            "dstaddr INT NOT NULL, " +
+                            "nexthop INT NOT NULL, " +
+                            "input SMALLINT NOT NULL, " +
+                            "output SMALLINT NOT NULL, " +
+                            "dPkts INT NOT NULL, " +
+                            "dOctets INT NOT NULL, " +
+                            "first INT NOT NULL, " +
+                            "last INT NOT NULL, " +
+                            "srcport SMALLINT NOT NULL, " +
+                            "dstport SMALLINT NOT NULL, " +
+                            "tcp_flags TINYINT NOT NULL, " +
+                            "prot TINYINT NOT NULL, " +
+                            "tos TINYINT NOT NULL, " +
+                            "src_as SMALLINT NOT NULL, " +
+                            "dst_as SMALLINT NOT NULL, " +
+                            "src_mask TINYINT NOT NULL, " +
+                            "dst_mask TINYINT NOT NULL, " +
+                            "flags SMALLINT NOT NULL, " +
+                            "router_sc INT NOT NULL, " +
+                            "PRIMARY KEY (id), " +
+                            "FOREIGN KEY (header_id) REFERENCES PACKET_V7_HEADER(id) ON DELETE CASCADE)";
+            stmt.executeUpdate(create);
+
+            /*create = "";
+            stmt.executeUpdate(create);
+
+            create = "";
+            stmt.executeUpdate(create);
+
+            create = "";
+            stmt.executeUpdate(create);
+
+            create = "";
+            stmt.executeUpdate(create);*/
+
+            return true;
+        }
+        catch (SQLException ex)
+        {
+            if (ex.getSQLState().equals("08S01"))
+            {
+                System.out.println("Error: Failed to connect to database with URL: " + url);
+            }
+            else
+            {
+                System.out.println("An unhandled error occured with the database\nFailed to connect to database with URL: " + url);
+            }
+            
+            System.out.println("\nFailed to create the database tables.\nRestart the program.");
+            return false;
+        }
+        catch (ClassNotFoundException ex)
+        {
+            System.out.println(ex.getMessage());
+            return false;
+        }
     }
 }
--- NetFlow/src/netflow/PacketV1.java
+++ NetFlow/src/netflow/PacketV1.java
@@ -8,6 +8,11 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 //http://netflow.caligare.com/netflow_v1.htm
 
@@ -33,42 +38,80 @@ public class PacketV1 extends Thread
     }
     
     /*
-     * Grabs all of the packet information.
+     * Grabs and saves all of the packet information.
      */
     private void SavePacket(DatagramPacket receivedPacket)
     {
         //Offset of 2 since version is being skipped.
-        try (ByteArrayInputStream byteIn = new ByteArrayInputStream(receivedPacket.getData(), 2, receivedPacket.getLength()); DataInputStream in = new DataInputStream(byteIn);)
+        try (ByteArrayInputStream byteIn = new ByteArrayInputStream(receivedPacket.getData(), 2, receivedPacket.getLength());
+                DataInputStream in = new DataInputStream(byteIn);)
         {
-            short count = in.readShort();
-            int sys_uptime = in.readInt();
-            int unix_secs = in.readInt();
-            int unix_nsecs = in.readInt();
-            in.skipBytes(1); //Skip unix_nsecs last bit (assumes only the first 4 are used)
-
-            for (int i = 0; i < count; i++)
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            try (Connection conn = DriverManager.getConnection("jdbc:mysql://" + Main.GetUrl(), Main.GetUser(), Main.GetPass());
+                Statement stmt = conn.createStatement();)
             {
-                int srcaddr = in.readInt();
-                int dstaddr = in.readInt();
-                int nexthop = in.readInt();
-                short input = in.readShort();
-                short output = in.readShort();
-                int dPkts = in.readInt();
-                int dOctets = in.readInt();
-                int first = in.readInt();
-                int last = in.readInt();
-                short srcport = in.readShort();
-                short dstport = in.readShort();
-                in.skipBytes(2); //Ignoring pad1
-                byte prot = in.readByte();
-                byte tos = in.readByte();
-                byte flags = in.readByte();
-                in.skipBytes(8); //Ignoring pad2
+                short count = in.readShort();
+                int sys_uptime = in.readInt();
+                int unix_secs = in.readInt();
+                int unix_nsecs = in.readInt();
+                in.skipBytes(1); //Skip unix_nsecs last bit (assumes only the first 4 are used)
+                
+                String insert = "INSERT INTO PACKET_V1_HEADER (count, sys_uptime, unix_secs, unix_nsecs) " +
+                        "VALUES (" + count + ", " + sys_uptime + ", " + unix_secs + ", " + unix_nsecs + ")";
+                
+                stmt.executeUpdate(insert, Statement.RETURN_GENERATED_KEYS);
+                ResultSet rs = stmt.getGeneratedKeys();
+                
+                if (rs != null && rs.next())
+                {
+                    int header_id = rs.getInt(1);
+                    
+                    for (int i = 0; i < count; i++)
+                    {
+                        int srcaddr = in.readInt();
+                        int dstaddr = in.readInt();
+                        int nexthop = in.readInt();
+                        short input = in.readShort();
+                        short output = in.readShort();
+                        int dPkts = in.readInt();
+                        int dOctets = in.readInt();
+                        int first = in.readInt();
+                        int last = in.readInt();
+                        short srcport = in.readShort();
+                        short dstport = in.readShort();
+                        in.skipBytes(2); //Ignoring pad1
+                        byte prot = in.readByte();
+                        byte tos = in.readByte();
+                        byte flags = in.readByte();
+                        in.skipBytes(8); //Ignoring pad2
+
+                        insert = "INSERT INTO PACKET_V1 (header_id, srcaddr, dstaddr, nexthop, input, output, dPkts, dOctets, first, last, srcport, dstport, prot, tos, flags) " +
+                                "VALUES (" + header_id + ", " + srcaddr + ", " + dstaddr + ", " + nexthop + ", " + input + ", " + output + ", " + dPkts + ", " + dOctets +
+                                 ", " + first + ", " + last + ", " + srcport + ", " + dstport + ", " + prot + ", " + tos + ", " + flags + ")";
+                        stmt.executeUpdate(insert);
+                    }
+                }
+                else
+                {
+                    System.out.println("Error: Unable to determine id of PACKET_V1_HEADER; associated flows dropped.");
+                }
+            }
+            catch (SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+                System.out.println("Error: An SQLException occurred in PacketV1.\n");
             }
         }
         catch (IOException ex)
         {
             System.out.println(ex.getMessage());
+            System.out.println("Error: An IOException occurred in PacketV1.\n");
+        }
+        catch (ClassNotFoundException ex)
+        {
+            System.out.println(ex.getMessage());
+            System.out.println("Error: A ClassNotFoundException occurred in PacketV1.\n");
         }
     }
 }
